e589f460227f01a4aa4dc52971115b01af3a36bb
cmsato09 <chrismsato@gmail.com>
2022-12-09 01:05:36 -0800

    wip: added some filtering

--- pages/2_monster_list.py
+++ pages/2_monster_list.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from helper_functions import get_monster_list
 
 # dummy data with test files
@@ -7,12 +8,13 @@ from helper_functions import get_monster_list
 
 # FastAPI connection
 monster_list = get_monster_list()
-
 st.markdown("## Monster List")
 name_search = st.text_input("Search by Name")
-family_search = st.selectbox("Search by Family",
-                             ['SLIME', 'DRAGON', 'BEAST', 'BIRD', 'PLANT',
-                              'BUG', 'DEVIL', 'UNDEAD', 'MATERIAL', '???'])
+family_search = st.selectbox(label="Search by Family",
+                             options=['SLIME', 'DRAGON', 'BEAST', 'BIRD',
+                                      'PLANT', 'BUG', 'DEVIL', 'UNDEAD',
+                                      'MATERIAL', '???'],
+                             )
 
 # Set up column table
 name_column, family_column = st.columns(2)
@@ -22,8 +24,10 @@ family_column.subheader('Family Type')
 for monster in monster_list:
     idx = monster['id']
     name = monster['old_name']
-    name_column.write(
-        f"<a target='_self' href='monster_detail?id={idx}'>{name}</a>",
-        unsafe_allow_html=True
-    )
-    family_column.write(monster['family']['family_eng'])
+    if family_search:
+        if monster['family']['family_eng'] == family_search:
+            name_column.write(
+                f"<a target='_self' href='monster_detail?id={idx}'>{name}</a>",
+                unsafe_allow_html=True
+            )
+            family_column.write(monster['family']['family_eng'])
--- pages/5_Item_list.py
+++ pages/5_Item_list.py
@@ -1,21 +1,28 @@
 import streamlit as st
 import pandas as pd
-from helper_functions import APINames, get_items_list
+from helper_functions import get_items_list
 
 # dummy data test
 # with open('json_test_files/items_dummy_data.json') as json_file:
 #     item_data = pd.read_json(json_file)
 
 if __name__ == "__main__":
-    st.markdown("## Items Table")
-    category_search = st.selectbox("Search by category",
-                                   ['recovery', 'meat', 'dungeon use', 'staff',
-                                    'book', 'seed'])
-    shop_search = st.selectbox("Search by shop",
-                               ['Bazaar shop 1', 'Bazaar shop 2',
-                                'Bazaar shop 3', 'Bazaar shop 4', 'Field shop',
-                                'found in field'])
     item_data = get_items_list()
+    df = pd.DataFrame(item_data)
+
+    st.markdown("## Items Table")
+    category_search = st.multiselect(label="Search by category",
+                                     options=df["item_category"].unique(),)
+    shop_search = st.multiselect(label="Search by category",
+                                 options=df["sell_location"].unique(),)
+    if category_search:
+        df = df.query("item_category == @category_search")
+    elif shop_search:
+        df = df.query("sell_location == @shop_search")
+    elif category_search and shop_search:
+        df = df.query(
+            "item_category == @category_search & sell_location == @shop_search"
+        )
 
     hide_table_row_index = """
                 <style>
@@ -24,8 +31,6 @@ if __name__ == "__main__":
                 </style>
                 """
     st.markdown(hide_table_row_index, unsafe_allow_html=True)
-
-    df = pd.DataFrame(item_data)
 
     df.rename(
         columns={
