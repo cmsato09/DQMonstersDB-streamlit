d54bd0069859e8a8f3b84f39d2610a92d17a29e7
cmsato09 <chrismsato@gmail.com>
2022-12-13 13:34:00 -0800

    added hyperlinks to 5_skill_detail page for upgrade to and from skill names

--- pages/5_skill_detail.py
+++ pages/5_skill_detail.py
@@ -1,12 +1,11 @@
 import streamlit as st
-import pandas as pd
 from helper_functions import get_skill_detail
 
 st.markdown('## Skill Detail Info Page Example')
 
 params = st.experimental_get_query_params()
 if "id" in params:
-    skill_id = params["id"]
+    skill_id = params["id"][0]
 else:
     skill_id = st.number_input(
         label="Input skill ID",
@@ -35,18 +34,31 @@ with row_value:
     st.write(f"{skill_data['mp_cost']}")
 
 if skill_data['upgrade_from']:
+    idx = skill_data['upgrade_from']['id']
+    upgrade_from_name = skill_data['upgrade_from']['old_name']
+
     row_header.write("Upgrades from :")
-    row_value.write(f"{skill_data['upgrade_from']['old_name']}")
+    row_value.write(
+        f"<a target='_self' href='skill_detail?id={idx}'>"
+        f"{upgrade_from_name}</a>",
+        unsafe_allow_html=True
+    )
 
 if skill_data['upgrade_to']:
+    idy = skill_data['upgrade_to']['id']
+    upgrade_to_name = skill_data['upgrade_to']['old_name']
     row_header.write("Upgrades to :")
-    row_value.write(f"{skill_data['upgrade_to']['old_name']}")
+    row_value.write(
+        f"<a target='_self' href='skill_detail?id={idy}'>"
+        f"{upgrade_to_name}</a>",
+        unsafe_allow_html=True
+    )
 
 st.write("Required stats to learn")
 
 lvl, hp, mp, attk, defense, spd, intel = st.columns(7, gap="small")
 with lvl:
-    st.write("<u>Level</u>", unsafe_allow_html=False)
+    st.write("Level")
     st.write(f"{skill_data['required_level']}")
 with hp:
     st.write("HP")
@@ -66,4 +78,3 @@ with spd:
 with intel:
     st.write("INT")
     st.write(f"{skill_data['required_intelligence']}")
-
