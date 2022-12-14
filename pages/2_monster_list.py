5fe1c4798716ea037231a38f977c9cc546d3ecf6
cmsato09 <chrismsato@gmail.com>
2022-12-14 15:40:40 -0800

    fix: added monster name search logic

--- pages/2_monster_list.py
+++ pages/2_monster_list.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import pandas as pd
 from helper_functions import get_monster_list
 
 # FastAPI connection
 monster_list = get_monster_list()
+
 st.markdown("## Monster List")
 name_search = st.text_input("Search by Name")
 family_search = st.selectbox(label="Search by Family",
@@ -17,19 +17,27 @@ name_column, family_column = st.columns(2)
 name_column.subheader('Monster Name')
 family_column.subheader('Family Type')
 
+
+def write_monster():
+    name_column.write(
+        f"<a target='_self' href='monster_detail?id={idx}'>{name}</a>",
+        unsafe_allow_html=True
+    )
+    family_column.write(monster['family']['family_eng'])
+
+
 for monster in monster_list:
     idx = monster['id']
     name = monster['old_name']
-    if family_search:
+
+    if name_search and family_search:
+        if name_search.lower() in name.lower() and monster['family']['family_eng'] == family_search:
+            write_monster()
+    elif name_search:
+        if name_search.lower() in name.lower():
+            write_monster()
+    elif family_search:
         if monster['family']['family_eng'] == family_search:
-            name_column.write(
-                f"<a target='_self' href='monster_detail?id={idx}'>{name}</a>",
-                unsafe_allow_html=True
-            )
-            family_column.write(monster['family']['family_eng'])
+            write_monster()
     else:
-        name_column.write(
-            f"<a target='_self' href='monster_detail?id={idx}'>{name}</a>",
-            unsafe_allow_html=True
-        )
-        family_column.write(monster['family']['family_eng'])
+        write_monster()
