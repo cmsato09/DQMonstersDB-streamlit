4c7b655496c365a3fe65825eb7bb8f628850c979
cmsato09 <chrismsato@gmail.com>
2023-01-08 21:43:25 -0800

    added info on the homepage

--- Home.py
+++ Home.py
@@ -2,22 +2,27 @@ import streamlit as st
 
 
 if __name__ == "__main__":
-    st.set_page_config(page_title="DQMDB", layout="centered",
-                       initial_sidebar_state="collapsed")
+    st.set_page_config(page_title="DQMDB", layout="centered")
     st.markdown("# Dragon Quest Monsters Database")
-    st.write(
-        f"<a target='_self' href='/About'> ABOUT </a>",
-        unsafe_allow_html=True
+
+    st.image(
+        "https://store-jp.nintendo.com/dw/image/v2/BFGJ_PRD/on/demandware.static/-/Sites-all-master-catalog/ja_JP/dwfe080eff/products/D70010000019662/heroBanner/054e861d9bc2a6077e838df18a21446d87ab4d2a815fb864854cafe69be593db.jpg?sw=1368&strip=false",
+        caption="Image from Nintendo Switch Japanese E-shop"
     )
     st.write(
-        f"<a target='_self' href='/monster_list'> Monster List </a>",
-        unsafe_allow_html=True
+        "Dragon Quest Monsters is a spin-off series based on the famous JRPG "
+        "series, Dragon Quest."
     )
     st.write(
-        f"<a target='_self' href='/skills_list'> Skills List </a>",
-        unsafe_allow_html=True
+        "This database informs the user of game details (monsters, breeding "
+        "combinations, skills, items, etc.) of Dragon Quest Monsters -- "
+        "Terry's Wonderland (also known as Dragon Warrior Monsters in the US)"
+        " **for the original GameBoy Color game** (not the new 3DS version)."
     )
     st.write(
-        f"<a target='_self' href='/item_list'> Item List </a>",
-        unsafe_allow_html=True
+        "If accessing this site on a mobile, open the collapsed sidebar by "
+        "clicking the arrow on the top left. "
+    )
+    st.write(
+        "All game images property of Square Enix"
     )
